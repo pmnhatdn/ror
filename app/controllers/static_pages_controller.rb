6633de5ef6384df46d2961ad7f9408794f464f06
PHAM MINH NHAT <pmnhatdn@gmail.com>
2013-04-12 17:07:14 -0700

    Initialize and static_pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def account
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoR::Application.config.secret_token = '90f5d0b6cbd39c6b64f3aeee9134f2c111f1e7d7eecb401d8cd2ec3e99b533e7f1a22daaf133c45cdeb8872837ce279bd975867c86dae8286dd13076ebdccf84'
